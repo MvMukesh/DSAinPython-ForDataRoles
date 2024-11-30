c43df2cfa09f20db0d8f9fa35c9756abd0f5a917
MvMukesh <mukeshmanral777@gmail.com>
2024-11-30 11:07:21 +0530

    ARR solution 3 updated

--- Algorithms/2Arrays/josephus.py
+++ Algorithms/2Arrays/josephus.py
@@ -11,10 +11,12 @@ Output: 369485271
 """
 
 def josephus(int_list, skip):
-    skip = skip - 1                     # list starts with 0 index
+    #list starts with 0 index
+    skip = skip - 1                     
     idx = 0
     len_list = (len(int_list))
     while len_list > 0:
-        idx = (skip + idx) % len_list   # hash index to every 3rd
+        #hash index to every 3rd
+        idx = (skip + idx) % len_list   
         yield int_list.pop(idx)
         len_list -= 1
