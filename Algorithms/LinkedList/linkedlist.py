d93950feaadeb498b0e492d1219659c507197d9e
MvMukesh <mukeshmanral777@gmail.com>
2024-11-30 10:31:10 +0530

    Skeleton (Singly - Doubly)

---
+++ Algorithms/LinkedList/linkedlist.py
@@ -0,0 +1,31 @@
+"""
+Author: https://www.linkedin.com/in/mukesh-manral/
+Representation of Singly and Doubly Linked List in Python
+"""
+
+""""
+# Pros
+* Linked Lists have constant-time insertions and deletions in any position,
+# in comparison, arrays require O(n) time to do the same thing.
+* Linked lists can continue to expand without having to specify
+# their size ahead of time (remember our lectures on Array sizing
+# from the Array Sequence section of the course!)
+"""
+"""
+# Cons
+* To access an element in a linked list, you need to take O(k) time
+# to go from the head of the list to the kth element.
+* In contrast, arrays have constant time operations to access
+# elements in an array.
+"""
+
+class SinglyLinkedListNode(object):
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class DoublyLinkedListNode(object):
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
