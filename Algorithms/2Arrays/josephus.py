ea4b8c8432c805b7130730b3067b4dd9706c9ba3
MvMukesh <mukeshmanral777@gmail.com>
2024-11-30 11:08:02 +0530

    ARR solution 4 updated

--- Algorithms/2Arrays/josephus.py
+++ Algorithms/2Arrays/josephus.py
@@ -2,7 +2,8 @@
 Author: https://www.linkedin.com/in/mukesh-manral/
 There are people sitting in a circular fashion,
 print every third member while removing them,
-the next counter starts immediately after the member is removed.
+the next counter starts immediately after 
+the member is removed.
 Print till all the members are exhausted.
 
 For example:
